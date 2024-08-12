d6268744de54297918c42a9e04085bdcc7806b0b
SOHEE JEONG <page.0895059@gmail.com>
2024-08-12 23:04:51 +0900

    Feat: Seller 모델 수정 및 Customer 모델 추가

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 
+from sqlalchemy import Column, String
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -7,10 +8,27 @@ class Seller(SQLModel, table=True):
     __tablename__ = "sellers"
 
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    email: str = Field(nullable=False, max_length=100, unique=True)
-    password: str = Field(nullable=False, max_length=256)
-    registration_number: str = Field(nullable=False, max_length=20, unique=True)
-    corporate_name: str = Field(nullable=False, max_length=20, unique=True)
-    contact_information: Optional[str] = Field(default=None, max_length=50)
+    email: str = Field(sa_column=Column(String(255), nullable=False, unique=True))
+    password: str = Field(sa_column=Column(String(255), nullable=False))
+    registration_number: str = Field(
+        sa_column=Column(String(20), nullable=False, unique=True)
+    )
+    corporate_name: str = Field(
+        sa_column=Column(String(50), nullable=False, unique=True)
+    )
+    contact_information: Optional[str] = Field(
+        sa_column=Column(String(20), nullable=False)
+    )
 
     products: List["Product"] = Relationship(back_populates="seller")
+
+
+class Customer(SQLModel, table=True):
+    __tablename__ = "customers"
+
+    id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    email: str = Field(sa_column=Column(String(255), nullable=False, unique=True))
+    password: str = Field(sa_column=Column(String(255), nullable=False))
+    name: str = Field(sa_column=Column(String(50), nullable=False))
+    phone_number: str = Field(sa_column=Column(String(20), nullable=False))
+    address: str = Field(sa_column=Column(String(200), nullable=False))
