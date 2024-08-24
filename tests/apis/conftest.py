dc478a23990575a162b92fe9bcfbea6f10cb298f
SOHEE JEONG <page.0895059@gmail.com>
2024-08-24 19:29:15 +0900

    Test: 상품 API에 대한 판매자 및 세션 검증 추가

--- tests/apis/conftest.py
+++ tests/apis/conftest.py
@@ -1,9 +1,13 @@
+import asyncio
+
 import pytest_asyncio
 from httpx import AsyncClient
+from redis.asyncio import Redis
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.database import close_db, create_db_and_tables, engine
 from src.main import app
+from src.redis_client import get_redis_client
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -18,3 +22,17 @@ async def client() -> AsyncClient:
 async def session() -> AsyncSession:
     async with AsyncSession(engine) as session:
         yield session
+
+
+@pytest_asyncio.fixture(scope="function")
+async def redis_client() -> Redis:
+    client = get_redis_client()
+    yield client
+    await client.close()
+
+
+@pytest_asyncio.fixture(scope="session")
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
