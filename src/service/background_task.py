379af8fc69335743be2bedd7ac8fd6920c5aa1c4
SOHEE JEONG <page.0895059@gmail.com>
2024-09-03 08:22:47 +0900

    Feat: 회원가입 시 이메일 발송 기능 구현

--- src/service/background_task.py
+++ src/service/background_task.py
@@ -1,5 +1,10 @@
 import asyncio
 import json
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from aiosmtplib import SMTP
 
 from src.elastic_client import get_elasticsearch_client
 from src.redis_client import get_task_redis_client
@@ -29,6 +34,11 @@ async def process_tasks():
                         if data:
                             product_info = json.loads(data)
                             await sync_product_to_elasticsearch(product_info)
+                    elif task_type == "send_email":
+                        data = message_data.get("data")
+                        if data:
+                            user_info = json.loads(data)
+                            await send_email(user_info["email"], user_info["name"])
 
                     # 메시지 처리 완료 후 ack (완료 처리함)
                     await task_redis.xack("task_stream", "task_group", message_id)
@@ -44,6 +54,12 @@ async def add_product_to_stream(product_info: dict):
     )
 
 
+async def add_email_to_stream(user_info: dict):
+    await task_redis.xadd(
+        "task_stream", {"type": "send_email", "data": json.dumps(user_info)}
+    )
+
+
 async def sync_product_to_elasticsearch(product_info: dict):
     try:
         await es.index(
@@ -54,3 +70,27 @@ async def sync_product_to_elasticsearch(product_info: dict):
         )
     except Exception as e:
         print(f"Error syncing product to Elasticsearch: {e}")
+
+
+async def send_email(email: str, name: str):
+    smtp_server = os.getenv("SMTP_SERVER")
+    smtp_port = os.getenv("SMTP_PORT")
+    smtp_user = os.getenv("SMTP_USER")
+    smtp_password = os.getenv("SMTP_PASSWORD")
+
+    msg = MIMEMultipart()
+    msg["From"] = smtp_user
+    msg["To"] = email
+    msg["Subject"] = "Welcome to our service!"
+
+    msg.attach(MIMEText(f"{name}, Thank you for register.", "plain"))
+
+    try:
+        async with SMTP(hostname=smtp_server, port=smtp_port) as client:
+            if smtp_port == 587:
+                await client.starttls()  # TLS 암호화 시작
+            await client.login(smtp_user, smtp_password)  # 로그인
+            await client.send_message(msg)  # 이메일 전송
+        print(f"Email sent to {email}")
+    except Exception as e:
+        print(f"Error sending email: {e}")
